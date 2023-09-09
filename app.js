76488b1394a7ea2a14734df4cc40dc51d0f3201e
stocaline <stocaline2@gmail.com>
2023-09-09 17:57:19 -0300

    finalizando estrutura

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+function sendMessage() {
+  var message = document.getElementById('message')
+  if (!message.value) {
+    message.style.border = '1px solid red'
+    return
+  }
+  message.style.border = 'none'
+
+  var status = document.getElementById('status')
+  var btnSubmit = document.getElementById('btn-submit')
+
+  status.style.display = 'block'
+  status.innerHTML = "Carregando..."
+  btnSubmit.disabled = true
+  btnSubmit.style.cursor = 'not-allowed'
+  message.disabled = true
+
+  fetch('https://chatbotfastapi.squareweb.app/processaPedido/', {
+    method: 'POST',
+    headers: {
+      //Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "prefixo": "8496",
+      "nomeContato": "Ibsem",
+      "mensagem": `${message.value}`
+    })
+  })
+  .then((response) => response.json())
+  .then((response) => {
+    var res = console.log(response)
+    showHistoric(message.value, rec)
+  })
+  .catch((e) => {
+    console.log('Error:', e)
+  })
+  .finally(() => {
+    status.style.display = 'none'
+    btnSubmit.disabled = false
+    btnSubmit.style.cursor = 'pointer'
+    message.disabled = false
+    message.value = ''
+  })
+
+
+  function showHistoric(message, response) {
+    var historic = document.getElementById('historic')
+
+    //My message
+    var boxMyMessage = document.createElement('div')
+    boxMyMessage.className = 'box-my-message'
+
+    var myMessage = document.createElement('p')
+    myMessage.className = 'my-message'
+    myMessage.innerHTML = message
+
+    boxMyMessage.appendChild(myMessage)
+    historic.appendChild(boxMyMessage)
+
+
+    //Response
+    var boxResponseMessage = document.createElement('div')
+    boxResponseMessage.className = 'box-response-message'
+
+    var chatResponse = document.createElement('p')
+    chatResponse.className = 'chat-response'
+    chatResponse.innerHTML = response
+
+    boxResponseMessage.appendChild(chatResponse)
+    historic.appendChild(boxResponseMessage)
+
+
+    historic.scrollTop = historic.scrollHeight
+  }
+}
+
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
@@ -5,4 +82,4 @@ if ("serviceWorker" in navigator) {
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
-  }
+}
