c86119fba81151fe2d3d9dbabcdbc201bef8e109
stocaline <stocaline2@gmail.com>
2023-09-09 14:25:57 -0300

    alternado para apenas uma pagina

--- index.js
+++ index.js
@@ -4,39 +4,52 @@ const path = require('path')
 const socketio = require('socket.io')
 
 
-app.use('/grupo1', express.static(path.join(__dirname, 'public')))
-app.use('/grupo2', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
+//app.use('/grupo1', express.static(path.join(__dirname, 'public')))
+//app.use('/grupo2', express.static(path.join(__dirname, 'public')))
 
 const server = app.listen(3000, () => {
     console.log("Running")
 })
 
-const messages = {
-    grupo1: [],
-    grupo2: [],
+const messages = []
 
-}
+// const messages = {
+//     grupo1: [],
+//     grupo2: [],
+// }
 
 const io = socketio(server)
 
-const grupo1 = io.of('/grupo1').on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log("new connection")
-    socket.emit("update_messages", messages.grupo1)
+    socket.emit("update_messages", messages)
 
     socket.on('new_message', (data) => {
-        messages.grupo1.push(data)
-        grupo1.emit('update_messages', messages.grupo1)
+        messages.push(data)
+        io.emit('update_messages', messages)
 
     })
 })
 
-const grupo2 = io.of('/grupo2').on('connection', (socket) => {
-    console.log("new connection")
-    socket.emit("update_messages", messages.grupo2)
+// const grupo1 = io.of('/grupo1').on('connection', (socket) => {
+//     console.log("new connection")
+//     socket.emit("update_messages", messages.grupo1)
 
-    socket.on('new_message', (data) => {
-        messages.grupo2.push(data)
-        grupo2.emit('update_messages', messages.grupo2)
+//     socket.on('new_message', (data) => {
+//         messages.grupo1.push(data)
+//         grupo1.emit('update_messages', messages.grupo1)
 
-    })
-})
+//     })
+// })
+
+// const grupo2 = io.of('/grupo2').on('connection', (socket) => {
+//     console.log("new connection")
+//     socket.emit("update_messages", messages.grupo2)
+
+//     socket.on('new_message', (data) => {
+//         messages.grupo2.push(data)
+//         grupo2.emit('update_messages', messages.grupo2)
+
+//     })
+// })
