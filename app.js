8444d2067f53d016d7b3ac7b31ec344d6d2a029c
RichardSB <richardbercheli@gmail.com>
2023-09-11 19:28:38 -0300

    Integração com api finalizada

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ function sendMessage() {
 
   fetch("https://chatbotfastapi.squareweb.app/processaPedido/", {
     method: "POST",
-    mode: "no-cors",
     headers: {
       "Content-Type": "application/json"
     },
@@ -27,59 +26,66 @@ function sendMessage() {
       "mensagem": `${message.value}`
     })
   })
-  .then((response) => {
-    console.log(response)
-    var res = JSON.parse(response.data).respostaBOT
-    showHistoric(message.value, res)
-  })
-  .catch((e) => {
-    console.log('Error:', e)
-  })
-  .finally(() => {
-    status.style.display = 'none'
-    btnSubmit.disabled = false
-    btnSubmit.style.cursor = 'pointer'
-    message.disabled = false
-    message.value = ''
-  })
-
-
-  function showHistoric(message, response) {
-    var historic = document.getElementById('historic')
-
-    //My message
-    var boxMyMessage = document.createElement('div')
-    boxMyMessage.className = 'box-my-message'
-
-    var myMessage = document.createElement('p')
-    myMessage.className = 'my-message'
-    myMessage.textContent = message
-
-    boxMyMessage.appendChild(myMessage)
-    historic.appendChild(boxMyMessage)
-
-
-    //Response
-    var boxResponseMessage = document.createElement('div')
-    boxResponseMessage.className = 'box-response-message'
-
-    var chatResponse = document.createElement('p')
-    chatResponse.className = 'chat-response'
-    chatResponse.innerHTML = response
-
-    boxResponseMessage.appendChild(chatResponse)
-    historic.appendChild(boxResponseMessage)
-
-
-    historic.scrollTop = historic.scrollHeight
-  }
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição');
+      }
+      return response.json()
+    })
+    .then((response) => {
+      console.log(response.respostaBOT)
+      var res = response.respostaBOT
+      showHistoric(message.value, res)
+    })
+    .catch((e) => {
+      console.log('Error:', e)
+      showHistoric(message.value, res)
+    })
+    .finally(() => {
+      status.style.display = 'none'
+      btnSubmit.disabled = false
+      btnSubmit.style.cursor = 'pointer'
+      message.disabled = false
+      message.value = ''
+    })
 }
 
+function showHistoric(message, response) {
+  var historic = document.getElementById('historic')
+
+  //My message
+  var boxMyMessage = document.createElement('div')
+  boxMyMessage.className = 'box-my-message'
+
+  var myMessage = document.createElement('p')
+  myMessage.className = 'my-message'
+  myMessage.innerHTML = message
+
+  boxMyMessage.appendChild(myMessage)
+  historic.appendChild(boxMyMessage)
+
+
+  //Response
+  var boxResponseMessage = document.createElement('div')
+  boxResponseMessage.className = 'box-response-message'
+
+  var chatResponse = document.createElement('p')
+  chatResponse.className = 'chat-response'
+  chatResponse.innerHTML = response
+
+  boxResponseMessage.appendChild(chatResponse)
+  historic.appendChild(boxResponseMessage)
+
+
+  historic.scrollTop = historic.scrollHeight
+}
+
+
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
 }
